d8ae3b561d8d24d3843cfdffd31e36fb1152f977
Łukasz Okrąglewski <202254@edu.p.lodz.pl>
2018-10-09 00:55:57 +0200

    basic program loop layout

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
-        Chip8 chip8;
+        Chip8 chip8 = new Chip8();
+        chip8.init();
 
+        for(;;)
+        {
+            chip8.emulate();
 
+//            if(Chip8.drawFlag)
+//                drawGraphics();
+//
+//            chip8.setKeys();
+        }
 
 
 
